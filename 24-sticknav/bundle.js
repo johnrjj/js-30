260e097dd1debeb7feceeb309e5873291e5fb183
John Johnson <johnrjj@gmail.com>
2016-12-26 15:33:21 -0500

    Fixed stickynav

--- 24-sticknav/bundle.js
+++ 24-sticknav/bundle.js
@@ -1,7 +1,14 @@
-window.addEventListener('scroll', () => {
-    const { offsetTop } = document.querySelector('#main');
-    console.log(offsetTop, window.scrollY);
-    return window.scrollY > offsetTop
-        ? document.body.classList.add('fixed-nav')
-        : document.body.classList.remove('fixed-nav');
+const getNavOffsetTop = () => {
+    const { offsetTop } = document.querySelector('.navbar');
+    return offsetTop;
+};
+const onScroll = () => (window.scrollY >= navOffsetTop)
+    ? document.body.classList.add('fixed-nav')
+    : document.body.classList.remove('fixed-nav');
+let navOffsetTop = getNavOffsetTop();
+window.addEventListener('resize', () => {
+    document.body.classList.remove('fixed-nav');
+    navOffsetTop = getNavOffsetTop();
+    onScroll();
 });
+window.addEventListener('scroll', onScroll);
--- 24-sticknav/bundle.ts
+++ 24-sticknav/bundle.ts
@@ -1,9 +1,19 @@
+const getNavOffsetTop = () => {
+  const { offsetTop } = document.querySelector('.navbar') as HTMLElement;
+  return offsetTop;
+};
 
-window.addEventListener('scroll', () => {
-  const { offsetTop } = document.querySelector('#main') as HTMLElement;
-  console.log(offsetTop, window.scrollY);
-  
-  return window.scrollY > offsetTop 
-    ? document.body.classList.add('fixed-nav')
-    : document.body.classList.remove('fixed-nav');
+const onScroll = () => (window.scrollY >= navOffsetTop)
+  ? document.body.classList.add('fixed-nav')
+  : document.body.classList.remove('fixed-nav');
+
+let navOffsetTop = getNavOffsetTop();
+
+window.addEventListener('resize', () => {
+  document.body.classList.remove('fixed-nav');
+  navOffsetTop = getNavOffsetTop();
+  onScroll();
 });
+
+window.addEventListener('scroll', onScroll);
+
